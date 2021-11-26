705d571f07861ddab1f088b1bf66304904f8963a
神之圍樓 <shenzhiweilou@gmail.com>
2021-11-26 20:21:01 +0800

    删除无用的APP

--- HoloCubic_Firmware/src/HoloCubic_AIO.cpp
+++ HoloCubic_Firmware/src/HoloCubic_AIO.cpp
@@ -18,14 +18,10 @@
 #include "app/weather/weather.h"
 #include "app/picture/picture.h"
 #include "app/media_player/media_player.h"
-#include "app/screen_share/screen_share.h"
-#include "app/bilibili_fans/bilibili.h"
 #include "app/server/server.h"
-#include "app/idea_anim/idea.h"
 #include "app/settings/settings.h"
 #include "app/file_manager/file_manager.h"
-#include "app/game_2048/game_2048.h"
-#include "app/weather_old/weather_old.h"
+
 
 /*** Component objects **7*/
 IMU mpu;
@@ -58,14 +54,9 @@ void setup()
     app_contorller->app_register(&weather_app);
     app_contorller->app_register(&picture_app);
     app_contorller->app_register(&media_app);
-    app_contorller->app_register(&screen_share_app);
     app_contorller->app_register(&server_app);
-    app_contorller->app_register(&idea_app);
-    app_contorller->app_register(&bilibili_app);
     app_contorller->app_register(&settings_app);
-    app_contorller->app_register(&game_2048_app);
     app_contorller->app_register(&file_manager_app);
-    app_contorller->app_register(&weather_old_app);
 
     // 优先显示屏幕 加快视觉上的开机时间
     app_contorller->main_process(&mpu.action_info);
